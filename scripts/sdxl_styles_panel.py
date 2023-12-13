bf79a7d3c8da82e6dcf7a5e56e2e13a359e5bf9e
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2023-12-14 00:24:21 +0100

    - removed unused code

--- scripts/sdxl_styles_panel.py
+++ scripts/sdxl_styles_panel.py
@@ -123,21 +123,22 @@ class SDXLStylesPanel(scripts.Script):
         p.extra_generation_params["SDXL Styles"] = styles
 
     def after_component(self, component, **kwargs):
-        # https://github.com/AUTOMATIC1111/stable-diffusion-webui/pull/7456#issuecomment-1414465888 helpful link
-        # Find the text2img textbox component
-        if kwargs.get("elem_id") == "txt2img_prompt":  # postive prompt textbox
-            self.boxx = component
-        # Find the img2img textbox component
-        if kwargs.get("elem_id") == "img2img_prompt":  # postive prompt textbox
-            self.boxxIMG = component
-
-        # this code below  works as well, you can send negative prompt text box,provided you change the code a little
-        # switch  self.boxx with  self.neg_prompt_boxTXT  and self.boxxIMG with self.neg_prompt_boxIMG
-
-        # if kwargs.get("elem_id") == "txt2img_neg_prompt":
-            #self.neg_prompt_boxTXT = component
-        # if kwargs.get("elem_id") == "img2img_neg_prompt":
-            #self.neg_prompt_boxIMG = component
+        pass
+        # # https://github.com/AUTOMATIC1111/stable-diffusion-webui/pull/7456#issuecomment-1414465888 helpful link
+        # # Find the text2img textbox component
+        # if kwargs.get("elem_id") == "txt2img_prompt":  # postive prompt textbox
+        #     self.boxx = component
+        # # Find the img2img textbox component
+        # if kwargs.get("elem_id") == "img2img_prompt":  # postive prompt textbox
+        #     self.boxxIMG = component
+        #
+        # # this code below  works as well, you can send negative prompt text box,provided you change the code a little
+        # # switch  self.boxx with  self.neg_prompt_boxTXT  and self.boxxIMG with self.neg_prompt_boxIMG
+        #
+        # # if kwargs.get("elem_id") == "txt2img_neg_prompt":
+        #     #self.neg_prompt_boxTXT = component
+        # # if kwargs.get("elem_id") == "img2img_neg_prompt":
+        #     #self.neg_prompt_boxIMG = component
 
 
 # def on_ui_settings():
